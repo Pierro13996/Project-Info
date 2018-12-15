a5521b3d11deacfbcdbb44354d47574f0534bd64
Pierro13996 <guyard.pierre01@gmail.com>
2018-12-15 14:12:48 +0100

    Main modif

--- Code/main.c
+++ Code/main.c
@@ -7,10 +7,6 @@ int main()
   printf("Modified");
   printf("Modified");
 
-<<<<<<< HEAD
-  printf("ATTENTION CA VA ETRE CHELOU");
-  
-=======
-  for(int i = 0; i<3; i++)printf("Itération %d\r\n");
->>>>>>> e31a2759a913e65485559b93aa9135186c18a833
+  printf("ATTENTION CA VA ETRE CHELOU 2");
+
 }
