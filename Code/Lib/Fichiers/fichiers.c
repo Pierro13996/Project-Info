0722e6a912c73193f710d7e6a3c6e671c8e057c3
blobdonekey <mathieu-ledimna@outlook.com>
2018-12-16 18:14:53 +0100

    Début de rédaction de fichiers.c et fichiers.h

--- Code/Lib/Fichiers/fichiers.c
+++ Code/Lib/Fichiers/fichiers.c
@@ -1,1 +1,20 @@
 #include "fichiers.h"
+
+FILE* ouvrir_fichier(char *chemin)
+{
+  FILE * fichier;
+
+  fichier = fopen(chemin, "a");
+
+  return fichier;
+}
+
+void fermer_fichier(void)
+{
+
+}
+
+void lire_fichier(char *buffer)
+{
+
+}
--- Code/Lib/Fichiers/fichiers.h
+++ Code/Lib/Fichiers/fichiers.h
@@ -1,6 +1,11 @@
 #ifndef FICHIERS_H
 #define FICHIERS_H
 
+#inclide "stdlib.h"
 
+FILE* ouvrir_fichier(char *chemin, char *nom);//Renvoie le pointeur du fichier
+void fermer_fichier(void);//Ferme le fichier
+
+void lire_fichier(char *buffer);//Stocke l'intégralité du fichier dans le buffer
 
 #endif
