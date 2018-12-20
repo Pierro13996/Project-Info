66505916d573d285faf71cfe2e72b35f7ee9dc83
blobdonekey <mathieu-ledimna@outlook.com>
2018-12-20 20:12:59 +0100

    Correction d'erreurs aberrantes

--- Code/Lib/Crash/crash.c
+++ Code/Lib/Crash/crash.c
@@ -1,40 +1,40 @@
 #include "crash.h"
 
-time_t lire_date(char *buffer, long *position)
+time_t lire_date(char *buffer, long *position)//C'est moins compliqué que ça en a l'air ~(°u°~)
 {
-  long position_initiale = *position;
+  long position_initiale = 0;
   char *buffer_date = NULL;
   char *buffer_heure = NULL;
 
-  position_initiale = *position;
+  position_initiale = *position;//On marque un repère
 
-  while(*buffer + *position != ',')*position++;
+  while(*buffer + *position != ',')*position++;//On incrémente tant qu'un séparateur n'a pas été vu
 
-  buffer_date = malloc( (*position - position_initiale) * sizeof(char)+1);
+  buffer_date = malloc( (*position - position_initiale) * sizeof(char)+1);//On alloue la mémoire nécessaire au stockage + un espace pour le '\0' (fin de chaîne)
 
-  if(buffer_date == NULL)
+  if(buffer_date == NULL)//Si la mémoire n'a pas étée alouée, on renvoie une erreur à l'utilisateur
   {
-    printf("Erreur (stocker_crashs) : Allocation mémoire impossible\r\n");
+    printf("Erreur (lire_date) : Allocation memoire impossible\r\n");
     exit(1);
   }
-  *position = position_initiale;
+  *position = position_initiale;//On se remet au début de l'info !
 
 
   //Année
-  position_initiale = *position;
-  while(*(buffer + *position) != '-')//Tant qu'il n'y a pas le séparateur
+  position_initiale = *position;//On marque un repère
+  while(*(buffer + *position) != '-')//Tant qu'il n'y a pas le séparateur on stocke nos données dans le buffer temporaire "buffer_date"
   {
     *(buffer_date + (*position - position_initiale)) = *(buffer+position);
     *position++;
   }
   *position++;
-  *(buffer_date + (*positon - position_initiale)) = '\0';
-  DateStruct.tm_year = atoi(buffer_date) - 1900;
+  *(buffer_date + (*positon - position_initiale)) = '\0';//On marque bien la fin de notre chaîne par un '\0'
+  DateStruct.tm_year = atoi(buffer_date) - 1900;//On convertit notre chaîne en un nombre décimal
 
 
   //Mois
   position_initiale = *position;
-  while(*(buffer + *position) != '-')//Tant qu'il n'y a pas le séparateur
+  while(*(buffer + *position) != '-')
   {
     *(buffer_date + (*position - position_initiale)) = *(buffer+position);
     *position++;
@@ -46,7 +46,7 @@ time_t lire_date(char *buffer, long *position)
 
   //Jour
   position_initiale = *position;
-  while(*(buffer + *position) != ',')//Tant qu'il n'y a pas le séparateur
+  while(*(buffer + *position) != ',')
   {
     *(buffer_date + (*position - position_initiale)) = *(buffer+position);
     *position++;
@@ -57,20 +57,23 @@ time_t lire_date(char *buffer, long *position)
 
   free(buffer_date);
 
+
+  position_initiale = *position;
+
   while(*buffer + *position != ',')*position++;
 
-  buffer_date = malloc( (*position - position_initiale) * sizeof(char)+1);
+  buffer_heure = malloc( (*position - position_initiale) * sizeof(char)+1);
 
-  if(buffer_date == NULL)
+  if(buffer_heure == NULL)
   {
-    printf("Erreur (stocker_crashs) : Allocation mémoire impossible\r\n");
+    printf("Erreur (lire_date) : Allocation memoire impossible\r\n");
     exit(1);
   }
-  *position++;
+  *position = position_initiale;
 
   //Heure
   position_initiale = *position;
-  while(*(buffer + *position) != ':')//Tant qu'il n'y a pas le séparateur
+  while(*(buffer + *position) != ':')
   {
     *(buffer_heure + (*position - position_initiale)) = *(buffer+position);
     *position++;
@@ -82,7 +85,7 @@ time_t lire_date(char *buffer, long *position)
 
   //Minute
   position_initiale = *position;
-  while(*(buffer + *position) != ',')//Tant qu'il n'y a pas le séparateur
+  while(*(buffer + *position) != ',')
   {
     *(buffer_heure + (*position - position_initiale)) = *(buffer+position);
     *position++;
@@ -94,7 +97,9 @@ time_t lire_date(char *buffer, long *position)
   DateStruct.tm_sec = 0;
   DateStruct.tm_isdst = -1;
 
-  return (time_t)mktime(&DateStruct);//Convertit la structure "DateStruct" en variable temporelle facile à manipuler
+  free(buffer_heure);
+
+  return (time_t)mktime(&DateStruct);//Convertit la structure "DateStruct" en variable temporelle facile à manipuler et la renvoie
 }
 
 void lire_lieu()
@@ -135,7 +140,7 @@ void stocker_crashs(char *buffer, TypeDef_Crash *Crashs, long nb_crash)
     buffer2 = malloc(6*sizeof(char));
     if(buffer2 == NULL)
     {
-      printf("Erreur (stocker_crashs) : Allocation mémoire impossible\r\n");
+      printf("Erreur (stocker_crashs) : Allocation memoire impossible\r\n");
       exit(1);
     }
 
