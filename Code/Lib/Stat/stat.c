f37bbfc315c7bf6646863f142c7c1e3b806497f2
Pierro13996 <guyard.pierre01@gmail.com>
2018-12-21 11:16:39 +0100

    Fonction nb crash /yr

--- Code/Lib/Stat/stat.c
+++ Code/Lib/Stat/stat.c
@@ -1,2 +1,13 @@
 #include "stat.h"
 
+int nb_crash_year(TypeDef_Crash *Crashs, int *nb_crash, int *year)
+{
+int nbcrash =0;
+{
+for(int i=0; i<=nb_crash; i++)
+{
+    if (Crashs[i].Annee==year) nbcrash++;
+}
+}
+    return nbcrash;
+}
--- Code/Lib/Stat/stat.h
+++ Code/Lib/Stat/stat.h
@@ -5,3 +5,4 @@
 #include <stdlib.h>
 #include "crash.h"
 
+int nb_crash_year(TypeDef_Crash *Crashs, int nb_crash, int *year);
