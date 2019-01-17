36b479471402bad4d84210c18a2e5f4381718a81
blobdonkey <mathieu-ledimna@outlook.com>
2019-01-17 13:28:11 +0100

    Inclusion de GSL OK

--- Code/Lib/Stat/stat.h
+++ Code/Lib/Stat/stat.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../../Src/main.h"
+#include <gsl/gsl_multifit.h>
+#include <math.h>
 
 void crashs_annee(TypeDef_Crash *, int );
 void nb_crashs_lieu(TypeDef_Crash *, int, int *, TypeDef_Crash *);
@@ -14,5 +16,7 @@ void crash_location_struct(TypeDef_Crash *, TypeDef_Crash *, int, char [20] );
 int nb_crashs_annee(TypeDef_Crash *Crashs, int nb_crash, int annee);
 int annee_min(TypeDef_Crash *Crashs, int nb_crash);
 int annee_max(TypeDef_Crash *Crashs, int nb_crash);
+
+void regression_polynomiale(int obs, int degree, double *dx, double *dy, double *store); /* n, p */
 
 #endif
